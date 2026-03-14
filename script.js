ab0bcc6574cacf3b1b50b2d89048d8d74ee29881
Sumit Kumar <smitk1057@gmail.com>
2026-03-14 10:06:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+
+    code.addEventListener("input", (e) => {
+        if(e.target.value.length === 1 && index < codes.length - 1){
+            codes[index + 1].focus();
+        }
+    });
+
+    code.addEventListener("keydown", (e) => {
+
+        if(e.key === "Backspace"){
+
+            if(code.value === "" && index > 0){
+                codes[index - 1].focus();
+            } else {
+                code.value = "";
+            }
+
+        }
+
+    });
+
+});
